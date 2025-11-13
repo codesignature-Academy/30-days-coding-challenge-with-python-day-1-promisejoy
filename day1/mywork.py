588541f3c4f948a6730c9e7fa32553de67f4e4e9
SmartDevTechs <smartdevtechs@gmail.com>
2025-11-13 15:13:17 +0100

    my assignment

---
+++ day1/mywork.py
@@ -0,0 +1,6 @@
+print("My Hobby Story")
+name = input("What's Your Name? ")
+hobby = input("What's Your Favourite Hobby? ")
+years_of_practice = input("How Many Years Have You Been Practicing? ")
+
+print(f"Hi! {name} Your New Hobby Is {hobby} \nYou Have Been Doing It For {years_of_practice}")
